d0c09a7a74afdcde2b1d275e63b03cb8fb1596c4
William <victorwccv@gmail.com>
2024-03-05 15:40:38 -0500

    navbar navegation complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Deposit from "./pages/Deposit";
 import Withdraw from "./pages/Withdraw";
 import Balance from "./pages/Balance";
 import AllData from "./pages/AllData";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/create-account" element={<CreateAccount/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/deposit" element={<Deposit/>} />
-        <Route path="/withdraw" element={<Withdraw/>} />
-        <Route path="/balance" element={<Balance/>} />
-        <Route path="/all-data" element={<AllData/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/balance" element={<Balance />} />
+        <Route path="/all-data" element={<AllData />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
 import logo from "../assets/Logo.png";
+import { Link, useLocation } from "react-router-dom";
 
 function NavBar() {
+  const location = useLocation();
+  const navLinks = [
+    { path: "/", text: "Home" },
+    { path: "/create-account", text: "Create Account" },
+    { path: "/login", text: "Login" },
+    { path: "/deposit", text: "Deposit" },
+    { path: "/withdraw", text: "Withdraw" },
+    { path: "/balance", text: "Balance" },
+    { path: "/all-data", text: "All Data" },
+  ];
+  console.log(location.pathname);
+
   return (
     <div>
-      <nav className="navbar navbar-expand-md bg-body-tertiary px-5 shadow">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow">
         <div className="container-fluid">
-          <a className="navbar-brand" style={{fontSize: '1.6rem'}} href="#">
+          <Link
+            to="/"
+            className="navbar-brand"
+            style={{ fontSize: "1.6rem" }}
+            href="#"
+          >
             <img
               src={logo}
               alt="Logo"
@@ -14,7 +32,7 @@ function NavBar() {
               className="d-inline-block align-text-top me-3"
             />
             Bad Bank App
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -27,19 +45,18 @@ function NavBar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-              <a className="nav-link" href="#">
-                Features
-              </a>
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
+            <div className="navbar-nav ms-auto">
+              {navLinks.map((link, index) => (
+                <Link
+                  key={index}
+                  to={link.path}
+                  className={`nav-link ${
+                    location.pathname === link.path ? "active" : ""
+                  }`}
+                >
+                  {link.text}
+                </Link>
+              ))}
             </div>
           </div>
         </div>
